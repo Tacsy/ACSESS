cb7ab5e4d0b04550fdbe9e15ad96bdc4b1512d07
Tacsy <lelielru@gmail.com>
2017-11-20 17:28:31 -0500

    add architecture of both init and diversity for filling

--- diversity.py
+++ diversity.py
@@ -1,3 +1,53 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+
+
+
+##############################
+# Check input
+##############################
+
+if mprms.restart: 
+    openmode = 'a'
+else:
+    openmode = 'w'
+
+##############################
+# Open output
+##############################
+
+filterFile = open('filters.dat', openmode)
+convergeFile = open('convergence.dat', openmode)
+statsFile = open('stats.dat', openmode)
+coordsStdDev = open('stddev.dat', openmode)
+
+statformat='{0:>7} {1:>8} {2:>8} {3:>10} {4:>10} {5:>10} {6:>11} {7:>7} {8:>9}'
+convergeformat='{0:>8} {1:>13} {2:>12} {3:>13} {4:>10} {5:>10}'
+if not mprms.restart:
+    print >> statsFile, statformat.format("#Gen","TooBig","Mutants",
+                                      "FailedMut","Undiverse","Filtered",
+                                      "Duplicates","Unfit","PoolSize")
+    print >> convergeFile, convergeformat.format(
+        '#- Round','-- Diversity','-- Max Atoms',
+        '-- SubsetSize','-- Filters','-- 3D Geom')
+
+
+iterhead="\n-------------------- Iteration {0} ----------------\n"
+
+##############################
+# Get starting library
+##############################
+
+
+
+
+
+###################################################
+##########                              ###########
+##########           MAIN LOOP          ###########
+##########                              ###########
+###################################################
+
+for gen in xrange(startiter, mprms.nGens):
+
--- init.py
+++ init.py
@@ -1,3 +1,38 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+
+
+######################################################
+# Read run parameters from mprms.py,
+# and store them all in StoredParams.py  
+######################################################
+def ReadMPRMS():
+    return
+
+
+
+
+
+
+
+
+######################################################
+# Get starting pool and library
+#
+#  Library comes from, in order of precedence:
+#      1. Restart files ('itX.lib.gz') (for restarts only)
+#      2. Read from mprms.seedfile
+#      3. Read from mprms.seedlib
+#      4. Preset in previous options are not met
+#         (benzene + cyclohexane)
+#
+#  Pool comes from:
+#      1. Restart file ('pool.lib.gz') (for restarts only)
+#      2. Read from mprms.poolfile
+#      3. Read from mprms.poollib
+#      4. Copy of library
+######################################################
+def StartLibrary(restart):
+    return 
+
