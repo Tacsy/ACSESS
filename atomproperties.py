d2cb3b0ad670ccdf31dba8ae5b57e1b19583d179
Tacsy <lelielru@gmail.com>
2017-12-12 16:34:24 -0500

    add ACVector and TSEI functions

--- atomproperties.py
+++ atomproperties.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+import numpy as np
+from rdkit import Chem
 import mprms
-
-
-
 
 
 
@@ -22,7 +21,7 @@ def AssignAtomicPolarizability(mol):
     
     for atom in mol.GetAtoms():
         #get degree for specific molecule
-        nBond = atom.GetTotalDegree()
+        nBonds = atom.GetTotalDegree()
 
         #Hydrogen (H)
         if atom.GetAtomicNum() == 1:
@@ -30,11 +29,11 @@ def AssignAtomicPolarizability(mol):
 
         #Carbon (C)
         elif atom.GetAtomicNum() == 6:
-            if nBond == 4:
+            if nBonds == 4:
                 atom.SetDoubleProp('polarizability', 1.294)
-            elif nBond == 2:
+            elif nBonds == 2:
                 GetAtoms.SetDoubleProp('polarizability', 1.393)
-            elif nBond == 3:
+            elif nBonds == 3:
                 if atom.GetNumExplicitHs() + atom.GetNumImplicitHs() > 0:
                     atom.SetDoubleProp('polarizability', 1.428)
                 else:
@@ -56,12 +55,12 @@ def AssignAtomicPolarizability(mol):
         #Nitrogen (N)
         elif atom.GetAtomicNum() == 7:
             if atom.GetIsAromatic():
-                if nBond == 2:
+                if nBonds == 2:
                     atom.SetDoubleProp('polarizability', 1.262)
                 else:
                     atom.SetDoubleProp('polarizability', 1.220)
             else:
-                if nBond == 1:
+                if nBonds == 1:
                     atom.SetDoubleProp('polarizability', 1.304)
                 else:
                     atom.SetDoubleProp('polarizability', 1.435)
@@ -71,7 +70,7 @@ def AssignAtomicPolarizability(mol):
             if atom.GetIsAromatic():
                 atom.SetDoubleProp('polarizability', 1.099)
             else:
-                if nBond == 1:
+                if nBonds == 1:
                     atom.SetDoubleProp('polarizability', 1.216)
                 else:
                     atom.SetDoubleProp('polarizability', 1.290)
@@ -80,22 +79,22 @@ def AssignAtomicPolarizability(mol):
         elif atom.GetAtomicNum() == 16:
             if atom.IsAromatic():
                 atom.SetDoubleProp('polarizability', 2.982)
-            elif nBond == 2:
+            elif nBonds == 2:
                 atom.SetDoubleProp('polarizability', 3.496)
             else:
                 atom.SetDoubleProp('polarizability', 3.967)
 
         #Halogens 
         elif atom.GetAtomicNum() == 9:
-            atom.SetDoubleProp('polarizability',1.046)
+            atom.SetDoubleProp('polarizability', 1.046)
         elif atom.GetAtomicNum() == 15:
-            atom.SetDoubleProp('polarizability',3.000)
+            atom.SetDoubleProp('polarizability', 3.000)
         elif atom.GetAtomicNum() == 17:
-            atom.SetDoubleProp('polarizability',3.130)
+            atom.SetDoubleProp('polarizability', 3.130)
         elif atom.GetAtomicNum() == 35:
-            atom.SetDoubleProp('polarizability',5.577)
+            atom.SetDoubleProp('polarizability', 5.577)
         elif atom.GetAtomicNum() == 53:
-            atom.SetDoubleProp('polarizability',8.820)
+            atom.SetDoubleProp('polarizability', 8.820)
                 
 	#Iridium (I)
 	#This param value was obtained by fitting the above known tau values
@@ -103,7 +102,7 @@ def AssignAtomicPolarizability(mol):
         #linear fit to get the value
 	#This is a crudest approx so could be wrong!
 	elif atom.GetAtomicNum() == 77:
-            atom.SetData('polarizability',12.77)
+            atom.SetData('polarizability', 12.77)
 
         else:
             raise KeyError('No polarizabilities for atomic number'
@@ -123,21 +122,99 @@ def AssignAtomicPolarizability(mol):
 # Second would be path termination length
 ############################################################
 def AssignTSEI(mol):
-    return
+    # all mol parsed into this function should be hydrogen added
 
+    radius = {1:0.315, 5:0.84, 6:0.772 ,7:0.711, 8:0.662, 9:0.57, 14:1.11,
+              15:1.06, 16:1.05, 17:1.024 ,35:1.14, 53: 1.33, 77:1.42}
+    nAtoms = mol.GetNumAtoms()
 
+    '''
+    this is a revision part of code that deviate from previous code in ACSESS,
+    we should consult with Aaron for more detail
+    '''
 
+    #Need to get shortest path between each pair of atoms 
+    #this could be faster by following paths outward from each atom
+    #if atom is hydrogen, the TSEI is set as 0.0 directly
+    for i in xrange(nAtoms):
+        iAtom = mol.GetAtomWithIdx(i)
+        if iAtom.GetAtomicNum() == 1:
+            iAtom.SetDoubleProp('TSEI',0.0)
+            continue
+        for j in xrange(i+1, nAtoms):
+            jAtom = mol.GetAtomWithIdx(j)
+            if jAtom.GetAtomicNum() == 1:
+                continue
+            else:
+                #calculate shoretest path
+                path = Chem.GetShortestPath(mol, i, j)
+                length = 0.0
+                for k in path:
+                    kAtom = mol.GetAtomWithIdx(k)
+                    if k == 0 or k == len(path)-1:
+                        length += radius[kAtom.GetAtomicNum()]
+                    else:
+                        length += 2.0*radius[kAtom.GetAtomicNum()]
+                
+                #factor of 2 cancels length vs. radius
+                iRad = radius[iAtom.GetAtomicNum()]
+                jRad = radius[jAtom.GetAtomicNum()]
+                if iAtom.HasProp('TSEI'):
+                    iAtom.SetDoubleProp('TSEI', iAtom.GetDoubleProp('TSEI')
+                                        +(2.0*jRad/length)**3)
+                else:
+                    iAtom.SetDoubleProp('TSEI', (2.0*jRad/length)**3)
+                if jAtom.HasProp('TSEI'):
+                    jAtom.SetDoubleProp('TSEI', jAtom.GetDoubleProp('TSEI')
+                                        +(2.0*iRad/length)**3)
+                else:
+                    jAtom.SetDoubleProp('TSEI', (2.0*iRad/length)**3)
+    
 
 ############################################################
 # Calculate autocorrelation vector for atomic properties
 # Method first described in Moreau and Broto. Nouv. J. Chim.1980, 4, 757-764.
 # A more recent reference is dx.doi.org/10.1002/poc.610061008
 ############################################################
-def AutoCorreationVector(mol, maxBonds, props):
-    return
+def AutoCorreationVector(mol, props, maxBonds):
+    
+    atoms = mol.GetAtoms()
+    
+    #Initializes the autocorrelation vector for each property
+    #To access the component for property x at bond separation y, use
+    #ACVector[x][y]
+    ACVector = [([0.0]*(maxBonds+1)) for prop in props]
 
+    #Retrive all properties for each atom
+    #atomProps[i][j] gives property j for itom i
+    atomProps = np.array([[atom.GetProp(prop) for prop in props] for atom in
+                         atoms])
+     
+    #construct autocorrelation vector
+    nAtoms = mol.GetNumAtoms()
+    for i in xrange(nAtoms):
+        iAtom = mol.GetAtomWithIdx(i)
+        for j in xrange(i, nAtoms):
+            jAtom = mol.GetAtomWithIdx(j)
+            
+            #get path length between this pair of atoms
+            if i != j:
+                pathlen = len(Chem.GetShortestPath(mol, i, j))-1
+                '''
+                this part is little bit confusing, we should consult with Aaron
+                for his advice on this
+                '''
+                if pathlen > maxBonds:
+                    pathlen = 0
+            else:
+                pathlen = 0
 
+            #add component to autocorrelation vectors
+            for iprop, prop in enumerate(props):
+                ACVector[iprop][pathlen] += (atomProps[i][iprop]*
+                                             atomProps[j][iprop])
 
+    return ACVector
 
 
 ############################################################
