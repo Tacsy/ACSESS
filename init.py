c38e55485db2665cf51af57b32c19b0b185437af
Tacsy <lelielru@gmail.com>
2017-12-14 16:22:44 -0500

    add initialization function

--- init.py
+++ init.py
@@ -19,7 +19,7 @@ def ReadMPRMS():
 
 
 ######################################################
-# Get starting pool and library
+# Get starting library and pool
 #
 #  Library comes from, in order of precedence:
 #      1. Restart files ('itX.lib.gz') (for restarts only)
@@ -34,32 +34,77 @@ def ReadMPRMS():
 #      3. Read from mprms.poollib
 #      4. Copy of library
 ######################################################
-def StartLibrary(restart):
-    
-    #case 1: restart file ('itX.lib.gz')
+def StartLibAndPool(restart):
 
+    if hasattr(mprms, 'nSeed'):
+        nSeed = mprms.nSeed
+    else:
+        nSeed = -1
+    
+    #####################
+    #   Start library   #
+    #####################
+
+    #case 1: restart file ('itX.lib.gz')
+    if 
     #case 2: read from mprms.seedfile
+    elif hasattr(mprms, 'seedFile'):
+        seedFile = mprms.seedFile
+        supplier = Chem.SmilesMolSupplier(seedFile)
+
+        lib = [mol for mol in supplier]
+        print "Seeding with " + str(len(lib)) + " molecules from " + seedFile
 
     #case 3: read from mprms.seedlib
-    
+    elif hasattr(mprms, 'seedLib'):
+        lib = mprms.seedLib
+        if nSeed > 0:
+            lib = lib[:nSeed]
+            lib = [Chem.MolFromSmiles(mol) for mol in lib]
+        if len(lib) == 0:
+            raise ValueError, "Seedlib is empty."
+        print "Seeding with mprms.seedlib"
+
     #case 4: predefined as (benzene + cyclohexane)
     else:
         print 'Seeding library with presets'
         lib = []
         lib.append(Chem.MolFromSmiles('C1CCCCC1'))
         lib.append(Chem.MolFromSmiles('C1=CC=CC=C1'))
-
-    return 
-
-
-def StartPool(restart):
+    
+    #####################
+    #     Start pool    #
+    #####################
     
     #case 1: restart file ('pool.lib.gz')
-
+    '''
+    may work together with starting library
+    '''
     #case 2: read from mprms.poolfile
+    if hasattr(mprms, 'poolFile'):
+        pool = [mol for mol in lib]
+        for poolFile in mprms.poolFile:
+            supplier = Chem.SmilesMolSupplier(poolFile)
+
+            newpool = [mol for mol in supplier]
+            pool += newpool
+
+            print 'Initializing pool from ' + poolFile + ' of ' +\
+                   str(len(newpool)) + ' molecules'
 
     #case 3: read from mprms.poollib
-
+    elif hasattr(mprms, 'poolLib'):
+        #poolLib is a list of SMILES
+        pool = mprms.poolLib
+        pool = [Chem.MolFromSmiles(mol) for mol in lib]
+        pool = pool + lib
+        if len(pool) == 0:
+            raise ValueError, "Poollib is empty."
+        print "Initializing pool from mprms.poolLib"
+    
     #case 4: copy from library
+    else:
+        print 'Initializing pool from library'
+        pool = [mol for mol in lib]
 
     return
