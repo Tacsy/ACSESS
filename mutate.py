9cd6d99b0a63e0c239e6f38a9d16f4bdc8d94b6f
Tacsy <lelielru@gmail.com>
2017-12-14 14:22:01 -0500

    init add output-related func

--- mutate.py
+++ mutate.py
@@ -1,3 +1,47 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import random
+from rdkit import Chem
+import mprms
+
+
+
+
+
+
+
+
+
+###############################################
+# Initialization the module
+###############################################
+
+def MutateInit():
+    elements = mprms.elements
+    halogens = [9,17,35,53]
+    global Elements, Halogens
+    
+    #do intersection and difference 
+    Elements = set(elements) - set(halogens)
+    Halogens = set(elements) & set(halogens)
+    #change from set to list
+    Elements = list(Elements)
+    Halogens = list(Halogens)
+    
+
+   
+###############################################################################
+#                            Mutation Methods                                 #
+###############################################################################
+
+
+###############################################
+#returns free valence for an atom
+#bbviously, there's a problem if it's negative
+#after implicit hydrogens are assigned with the valence model,
+#this can be replaced by the implicit hydrogen count
+###############################################
+
+
+
--- output.py
+++ output.py
@@ -1,3 +1,126 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import time
+import sys
+
+startTime = time.time()
+
+##########################
+# Statistics function
+##########################
+
+statistics = {}
+def PrintStat(nColumn, flush=True):
+    global statistics
+    if len(statistics) == 0:
+        return
+    PrintDict(statistics, nColumn)
+    if flush:
+        statistics = {}
+    sys.stdout.flush()
+
+##########################
+# Timer function
+##########################
+
+timingHead = "\n    TIMINGS:\n"
+totalTimingHead = """\n
+--------------------------------------------------------
+                        TOTAL TIMINGS                   
+--------------------------------------------------------\n"""
+
+timings = {}
+timeRunning = {}
+totalTimes = {}
+
+def StartTimer(key):
+    global timeRunning
+    if debug:
+        print "Starting timer: " + key
+    if timeRunning.has_key(key):
+        print "WARNING: " + key + "already running!"
+    else:
+        timeRunning[key] = time.time()
+
+def EndTimer(key):
+    global timeRunning, timings 
+    if debug:
+        print "Ending timer: " + key
+    #get runtime 
+    runTime = time.time() - timeRunning[key]
+    timeRunning.pop(key)
+    if timings.has_key(key):
+        timings[key] += runTime
+    else:
+        timings[key] = runTime
+
+def PrintTimings(nColumn, flush = True):
+    global timings, totalTimes
+    keys = timings.keys()
+    if len(keys) == 0:
+        return
+    values = timings.values()
+    print timingHead
+    PrintDict(timings, nColumn, truncate = True, sort = 'val')
+    if flush:
+        for key, value in timings.iteritems():
+            if totalTimes.has_key(key):
+                totalTimes[key] += value
+            else:
+                totalTimes[key] = value
+        timings = {}
+    
+    '''
+    debug part should be considered
+    '''
+
+def PrintTotalTimings(nColumn):
+    global totalTimes
+    print totalTimingHead
+    if len(totalTimes) == 0:
+        return
+    PrintDict(totalTimes, truncate = True, sort = 'val')
+
+
+##########################
+# Printing function
+##########################
+
+def PrintDcit(mydict, nColumn, truncate = False, sort = False):
+    if sort != False:
+        if sort == 'val':
+            idx = 1
+        elif sort  == 'key':
+            idx = 0
+        keys, values = zip(*sorted(mydict.items(), key = lambda x: -x[idx]))
+        keys = list(keys)
+        values = list(values)
+    else:
+        keys = mydict.keys()
+        values = mydict.values()
+
+    if truncate:
+        values = ["%.2f"%v for v in values]
+    
+    #complation of keys/values in the fashion that fits the format
+    while len(keys)%nColumn != 0:
+        keys.append(' ')
+        values.append(' ')
+
+    #formating key and value in row
+    keyformat = ''
+    valformat = ''
+    for i in xrange(nColumn):
+        keyformat += '+--{:<15}--'
+        valformat += '|  {:<15}}  '
+    keyformat += '+'
+    valformat += '|'
+
+    for i in xrange(0, len(keys), nColumn):
+        print keyformat.format(*keys[i:i+nColumn])
+        print valformat.format(*values[i:i+nColumn])
+
+    sys.stdout.flush()
+
+
