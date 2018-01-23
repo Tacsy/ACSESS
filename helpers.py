64e82f35a329b0d9a9675552a66d60f11b35b659
Tacsy <lelielru@gmail.com>
2018-01-23 17:46:52 -0500

    init helpers module

---
+++ helpers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import os
+import sys
+
+'''
+this is the helper module that contains helping functions that relates to
+both chemical and non-chemical perspectives
+'''
+
+
