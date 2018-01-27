34e9a9498cc000dec46e233313bcbd9d9e9d65af
Tacsy <lelielru@gmail.com>
2018-01-27 17:17:32 -0500

    add qiu filters

--- filters.py
+++ filters.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+from rdkit import Chem
+from rdkit.Chem import AllChem
 
 
 '''
@@ -15,3 +17,44 @@ previous filters included:
     Filters_BC.py
     QiuFilter.py
 '''
+
+############################################################
+#       Functions from QiuFilter.py
+############################################################
+
+# ensure molecules do not have ring with ring size N
+def CheckRingSize(molList, ringSize):
+    # get mol list and return list of filterd molecules
+    filtered = []
+    
+    for mol in molList:
+        dumpMol = False
+        # calculate smallest set of rings (SSR)
+        ssr = Chem.GetSymmSSSR(mol)
+        for i in range(len(ssr)):
+            if len(list(ssr[i])) == ringSize:
+                dumpMol = True
+
+        if dumpMol:
+            filtered.append(mol)
+    
+    return filtered
+
+# ensure molecules do not have a bond with bondorder N
+def CheckBondOrder(molList, bondOrder):
+    # get mol list and return list of filtered molecules
+    # bondOrder should be double, 1.0 for single, 1.5 for aromatic, 2.0 for
+    # double, 3.0 for triple
+    filtered = []
+
+    for mol in molList:
+        dumpMol = False
+        # loop over bonds in molecule and check bond order
+        for bond in mol.GetBonds():
+            if bond.GetBondTypeAsDouble() == bondOrder:
+                dumpMol = True
+
+        if dumpMol:
+            filtered.append(mol)
+
+    return filtered
